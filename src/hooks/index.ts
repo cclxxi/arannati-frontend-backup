710997c5c9feef05d0e8c843795a19dcd2e0d825
Ilia_P <ilia.proshin82@gmail.com>
2025-07-15 19:55:36 +0600

    feat(hooks): aggregate and re-export React Query hooks through a centralized module

---
+++ src/hooks/index.ts
@@ -0,0 +1,73 @@
+// Auth hooks
+export {
+    useCurrentUser,
+    useLogin,
+    useRegister,
+    useRegisterCosmetologist,
+    useLogout,
+    useForgotPassword,
+    useResetPassword,
+    useIsAuthenticated,
+    useHasRole,
+} from './queries/useAuth';
+
+// Catalog hooks
+export {
+    useProducts,
+    useInfiniteProducts,
+    useProduct,
+    useProductSearch,
+    usePopularProducts,
+    useSaleProducts,
+    useNewProducts,
+    useSimilarProducts,
+    usePrefetchProduct,
+} from './queries/useCatalog';
+
+// Cart hooks
+export {
+    useCartItems,
+    useCartCount,
+    useCartSummary,
+    useAddToCart,
+    useUpdateCartItem,
+    useRemoveFromCart,
+    useClearCart,
+    useValidateCart,
+} from './queries/useCart';
+
+// Wishlist hooks
+export {
+    useWishlistItems,
+    useWishlistCount,
+    useIsInWishlist,
+    useAddToWishlist,
+    useRemoveFromWishlist,
+    useToggleWishlist,
+    useAddAllToCart,
+} from './queries/useWishlist';
+
+// Orders hooks
+export {
+    useOrders,
+    useOrderDetails,
+    useCheckoutSummary,
+    useShippingCalculation,
+    useCreateOrder,
+    useCancelOrder,
+    useRepeatOrder,
+    useRecentOrders,
+    useOrderStats,
+    usePrefetchOrder,
+} from './queries/useOrders';
+
+// Messages hooks
+export {
+    useChats,
+    useChatMessages,
+    useSendMessage,
+    useSendSupportRequest,
+    useMarkChatAsRead,
+    useUnreadMessagesCount,
+    useTypingIndicator,
+} from './queries/useMessages';
